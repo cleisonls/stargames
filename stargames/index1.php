e9fb1dac4d2f6a041730374e1a449b9014c9dc8b
cleisonls <44264186+cleisonls@users.noreply.github.com>
2018-11-28 11:51:10 -0200

    stargames
    aaa

---
+++ stargames/index1.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+		<title>StarGames</title>
+	</head>
+	<body>
+	<div class="container-fluid">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Análises</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Todos os jogos
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">aaa</a>
+							<a class="dropdown-item" href="#">aaa</a>
+							<!--<div class="dropdown-divider"></div>-->
+							<a class="dropdown-item" href="#">aaa</a>
+							<a class="dropdown-item" href="#">aaa</a>
+							<a class="dropdown-item" href="#">aaa</a>
+							<a class="dropdown-item" href="#">aaa</a>
+						</div>
+					</li>
+				</ul>
+				<div class="form-inline my-2 my-lg-0">
+					<ul class="nav navbar">
+						<a href="" class="nav-link"><li class="nav-item">Cadastre-se</li></a>|
+						<a href="" class="nav-link"><li class="nav-item">Login</li></a>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+		
+	
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 text-center" style="background:#ccc; height:350px">
+					<h2> um banner muito bonito aqui</h2>
+			</div>
+		</div>
+	</div>
+		<br><hr><br>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3" style="height:250px;">
+				<div class="card" style="width: 18rem;">
+					<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3" style="height:250px;">
+				<div class="card" style="width: 18rem;">
+					<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3" style="height:250px;">
+				<div class="card" style="width: 18rem;">
+					<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>	
+			</div>
+			<div class="col-md-3" style="height:250px;">
+				<div class="card" style="width: 18rem;">
+					<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	<br><hr><br><br><br><br><br>
+	<footer class="container-fluid" id="rodape" style="background:#ccc;height: 85px;font-family: Montserrat,-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol','Noto Color Emoji';">
+			<div class="row">
+				<div class="col-md-2">
+					logo aqui
+				</div>
+				<div class="col-md-2">
+					<span><i class="fas fa-phone" style="font-size:20px;"></i>&nbsp;&nbsp;(21)&nbsp;4002-8922</span>
+				</div>
+				<div class="col-md-4">
+					<span style="font-size:0.9em">COPYRIGHT © 2018 Todos os direitos reservados.</span>
+				</div>
+				<div class="col-md-2">
+					<span><i class="fab fa-instagram" style="font-size:25px;"></i>&nbsp;&nbsp;@stargames</span>
+				</div>
+				<div class="col-md-2">
+					<span><i class="fab fa-facebook" style="font-size:25px;"></i>&nbsp;&nbsp;/stargames.com</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">&nbsp;<br></div>
+			</div>
+	</footer>
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		
+	</body>
+</html>
