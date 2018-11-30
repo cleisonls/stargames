dd2a0d26237d33bcc3bf7671dc25883683f06171
cleisonls <44264186+cleisonls@users.noreply.github.com>
2018-11-30 11:54:14 -0200

    aaa

--- stargames/index1.php
+++ stargames/index1.php
@@ -4,108 +4,227 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
+		<link rel="stylesheet" href="assets/css/index1.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 		<title>StarGames</title>
 	</head>
 	<body>
-	<div class="container-fluid">
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Análises</a>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Todos os jogos
-						</a>
-						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="#">aaa</a>
-							<a class="dropdown-item" href="#">aaa</a>
-							<!--<div class="dropdown-divider"></div>-->
-							<a class="dropdown-item" href="#">aaa</a>
-							<a class="dropdown-item" href="#">aaa</a>
-							<a class="dropdown-item" href="#">aaa</a>
-							<a class="dropdown-item" href="#">aaa</a>
-						</div>
-					</li>
-				</ul>
-				<div class="form-inline my-2 my-lg-0">
-					<ul class="nav navbar">
-						<a href="" class="nav-link"><li class="nav-item">Cadastre-se</li></a>|
-						<a href="" class="nav-link"><li class="nav-item">Login</li></a>
+	<!---menu navbar---------------->
+	
+		<nav class="navbar navbar-expand-lg" style="background:#000;">
+			<div class="container">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
+				<span class="navbar-toggler-icon" style="color:white;"><i class="fas fa-bars"></i></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item active">
+							<img src="assets/img/starlogo.png" style="width:40px; margin-right:8px;"/>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Análises</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Todos os jogos
+							</a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item" href="#">aaa</a>
+								<a class="dropdown-item" href="#">aaa</a>
+								<!--<div class="dropdown-divider"></div>-->
+								<a class="dropdown-item" href="#">aaa</a>
+								<a class="dropdown-item" href="#">aaa</a>
+								<a class="dropdown-item" href="#">aaa</a>
+								<a class="dropdown-item" href="#">aaa</a>
+							</div>
+						</li>
 					</ul>
+					<div class="form-inline my-2 my-lg-0">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item">
+								<a href="" class="nav-link"><li class="nav-item">Cadastre-se</li></a>
+							</li>
+							<li class="nav-item">
+								<a href="" class="nav-link"><li class="nav-item">Login</li></a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</nav>
-	</div>
 		
-	
+	<!---banner ------------------>
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-12 text-center" style="background:#ccc; height:350px">
-					<h2> um banner muito bonito aqui</h2>
+			<div class="col-md-12 text-center" id="topo" style=" height:350px">
+					<div class="container-fluid">
+		<div id="banner">
+        <center>
+			<div class="col-12 col-sm-4 col-md-8">
+				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="margin-top: 40px;">
+					<ol class="carousel-indicators">
+					  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+					  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+					  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					  <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+					</ol>
+					<div class="carousel-inner">
+					  <div class="carousel-item active">
+						<img class="d-block w-100" src="assets/img/banner1.jpg" alt="First slide">
+					  </div>
+					  <div class="carousel-item">
+						<img class="d-block w-100" src="assets/img/banner2.jpg" alt="Second slide">
+					  </div>
+					  <div class="carousel-item">
+						<img class="d-block w-100" src="assets/img/banner3.jpg" alt="Third slide">
+					  </div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+					  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					  <span class="sr-only">Anterior</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+					  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+					  <span class="sr-only">Próximo</span>
+					</a>
+				</div>
+            </div>
+        </center>
+    </div>
 			</div>
 		</div>
 	</div>
-		<br><hr><br>
+		<br><hr><br><br><br><br><br><br><br><br><br><br><br>
+		
+	<!---conteudo---------------->
 	<div class="container">
+	<div class="col-12">
 		<div class="row">
-			<div class="col-md-3" style="height:250px;">
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
+		<!--------conteudo esquerda--------------------------------->
+			<div class="col-md-8">
+				<div class="card" style="width:650px; height:200px;">
+					<div class="row">
+						<div class="col-5">
+							<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
+						</div>
+						<div class="col-7">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>É o Miranha</h5>
+								<p>Spoder-Man Appears</p>
+							  </div>
+						</div>
+					</div>
+				</div>
+			
+				<div class="card" style="width:650px; height:200px;">
+					<div class="row">
+						<div class="col-5">
+							<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
+						</div>
+						<div class="col-7">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			
+				<div class="card" style="width:650px; height:200px;">
+					<div class="row">
+						<div class="col-5">
+							<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
+						</div>
+						<div class="col-7">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div class="card" style="width:650px; height:200px;">
+					<div class="row">
+						<div class="col-5">
+							<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
+						</div>
+						<div class="col-7">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-3" style="height:250px;">
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
+			
+			<!--------conteudo direita--------------------------------->
+			<div class="col-md-4" style="background:#ccc">
+				<div class="analise">
+					<div><h2>Análises</h2></div>
+					<div class="caixaAnalise" style="background:#fff">
+						<div class="tituloAnalise">
+							<h5>GTA 5</h5>
+						</div>
+						<div class="plataformaAnalise">
+							PC
+						</div>
 					</div>
-				</div>
-			</div>
-			<div class="col-md-3" style="height:250px;">
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
+					<hr>
+					<div class="caixaAnalise" style="background:#fff">
+						<div class="tituloAnalise">
+							<h5>GTA 5</h5>
+						</div>
+						<div class="plataformaAnalise">
+							PC
+						</div>
 					</div>
-				</div>	
-			</div>
-			<div class="col-md-3" style="height:250px;">
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="assets/img/semfoto.jpg" alt="Card image cap">
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
+					<hr>
+					<div class="caixaAnalise" style="background:#fff">
+						<div class="tituloAnalise">
+							<h5>GTA 5</h5>
+						</div>
+						<div class="plataformaAnalise">
+							PC
+						</div>
+					</div>
+					<hr>
+					<div class="caixaAnalise" style="background:#fff">
+						<div class="tituloAnalise">
+							<h5>GTA 5</h5>
+						</div>
+						<div class="plataformaAnalise">
+							PC
+						</div>
+					</div>
+					<hr>
+					<div class="caixaAnalise" style="background:#fff">
+						<div class="tituloAnalise">
+							<h5>GTA 5</h5>
+						</div>
+						<div class="plataformaAnalise">
+							PC
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+	</div>
 	
-	
-	<br><hr><br><br><br><br><br>
+	<br><br><br><br><hr>
+	<!---footer---------------->
 	<footer class="container-fluid" id="rodape" style="background:#ccc;height: 85px;font-family: Montserrat,-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol','Noto Color Emoji';">
 			<div class="row">
 				<div class="col-md-2">
-					logo aqui
+					<img src="assets/img/starlogo.png" style="width:40px"/>
 				</div>
 				<div class="col-md-2">
 					<span><i class="fas fa-phone" style="font-size:20px;"></i>&nbsp;&nbsp;(21)&nbsp;4002-8922</span>
@@ -128,13 +247,7 @@
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
+	
 		
 		
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
